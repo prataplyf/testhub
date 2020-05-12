7c84e6b4bd19c2522193ad24cae7d6ceb5d24c52
prataplyf <prataplyf@gmail.com>
2020-05-12 17:16:06 +0530

    update

--- hub.py
+++ hub.py
@@ -3,4 +3,4 @@ from wshub import app
 from werkzeug.serving import run_simple
 
 if __name__ == "__main__":
-    run_simple('localhost', 5000, app, use_reloader= True, use_debugger = True, use_evalex=True)
+    run_simple('127.0.0.1', 8000, app, use_reloader= True, use_debugger = True, use_evalex=True)
